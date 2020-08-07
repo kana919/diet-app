12bca74c2431d75347005b8a459028c7d12acef0
kana919 <kaitakana.3093@gmail.com>
2020-08-07 21:50:39 +0900

    タイトル・行頭イラスト追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <section class="container">
-    <h1 class="title">
-      {{ nowdate }}  {{ usrname }}さんの記録
-    </h1>
+    <div class="title-box">
+      <h1 class="title">
+        {{ nowdate }}  {{ usrname }}さんの記録
+      </h1>
+    </div>
     <div class="usrdata-box">
+      <h2></h2>
       <ul>
         <li>身長：{{ usrHeight }}cm</li>
         <li>体重：{{ usrWeight }}kg</li>
@@ -38,9 +41,6 @@ export default {
     usrWeight:0,
     usrBmi:0,
     usrFat:0
-  }),
-  methods: ()=>({
-
   })
 }
 </script>
@@ -48,16 +48,66 @@ export default {
 <style>
 .container {
   min-height: 100vh;
+  min-width: 400px;
   display: block;
   text-align: left;
   color: #f08080;
   background-color: #fff5ee;
+  border: 35px solid transparent;
 }
 
-.title-block{
-  display: block;
-  font-size: 20px;
-  text-align: left;
+.container ul{
+  list-style: none;
 }
+
+/**********************************/
+/*          　 タイトル             */
+/**********************************/
+
+.title-box{
+  display: block;
+  width: inherit;
+  height: 100px;
+  background-image: url('~assets/image/title.png');
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+
+.title{
+  text-align: center;
+  width: inherit;
+  line-height: 3.5em;
+}
+
+/**********************************/
+/*         ユーザーデータ           */
+/**********************************/
+
+.usrdata-box{
+  font-size: 20px;
+  margin: 5px;
+  padding: 10px;
+  border: 2px solid #f08080;
+  border-radius: 5px;
+}
+
+.usrdata-box li{
+  padding-left: 28px;
+	line-height: 1.6em;
+  background: url('~assets/image/list-illust1.png') left 0px top 2px no-repeat;
+  background-size: 28px auto;
+}
+
+/**********************************/
+/*         筋トレメニュー           */
+/**********************************/
+.training-box{
+  font-size: 20px;
+  padding: 10px;
+  border: 2px solid #f08080;
+  border-radius: 5px;
+}
+
+
 </style>
 
